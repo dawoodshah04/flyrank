3532cfbb8f45f84d30044eef06150b752bbdc297
dawood shah <dawood0963@gmail.com>
2026-07-19 14:15:13 +0500

    Stage 1: root and health endpoints

--- week-1/main.py
+++ week-1/main.py
@@ -1,8 +1,38 @@
+from http.client import HTTPException
+
 from fastapi import FastAPI
+import uuid
 
 
 app = FastAPI()
 
+
+tasks = [
+{
+    "id": uuid.uuid4(),
+    "title":"get request",
+    "done":True
+
+},
+{
+    "id": uuid.uuid4(),
+    "title":"POST:id request",
+    "done":True
+
+},
+{
+    "id": uuid.uuid4(),
+    "title":"POST request",
+    "done":False
+
+},
+
+]
+
 @app.get('/')
 async def root():
-    return {"message:","Hello, World"}
+    return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+
+@app.get('/health')
+async def root():
+    return {"status":"ok"}
