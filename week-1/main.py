cb0cc3d5fa64ad22ad2277e67624ede0237c126f
dawood shah <dawood0963@gmail.com>
2026-07-19 14:15:24 +0500

    Stage 2: read endpoints with 404

--- week-1/main.py
+++ week-1/main.py
@@ -36,3 +36,15 @@ async def root():
 @app.get('/health')
 async def root():
     return {"status":"ok"}
+
+@app.get('/tasks')
+async def root():
+    return tasks
+
+@app.get('/tasks/{id}')
+async def root(id:uuid.UUID):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+        raise HTTPException(status_code=404,details=f"Task{id} not Found") 
+        
