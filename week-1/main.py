1c33f7dc8af840952ab1e1ee585b5a5b2246ed3e
dawood shah <dawood0963@gmail.com>
2026-07-27 17:53:50 +0500

    Stage 4: full CRUD

--- week-1/main.py
+++ week-1/main.py
@@ -1,11 +1,18 @@
 from http.client import HTTPException
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import Response
 import uuid
+
 
 class TaskCreate(BaseModel):
     title:str
     done:bool=False
+
+class TaskUpdate(BaseModel):
+    title:str
+    done:bool
+
 
 app = FastAPI()
 
@@ -53,14 +60,46 @@ async def root(id:uuid.UUID):
 
 
 @app.post('/tasks')
-async def create_task(task:TaskCreate):
+async def create_task(task:TaskCreate, res:Response):
+    
     new_task = {
         "id" : uuid.uuid4(),
         "title" : task.title,
         "done" : False,
     }
-
     tasks.append(new_task)
+    if not new_task:
+        raise HTTPException(status_code=404,details=f"Task Name not added")
 
-    return new_task
+    res.status_code = 201
 
+    return {
+        "message": "done, here's your receipt",
+        "data": new_task,
+        "status_code":201
+    }
+
+
+@app.put('/tasks/{id}')
+async def update_res(task: TaskUpdate,id:uuid.UUID):
+    if not task.title or task.title.strip() == "":
+        raise HTTPException(status_code=400, detail=f"Title required")
+    for t in tasks:
+        if t["id"] == id:
+            t["title"] = task.title
+            t["done"] = task.done
+            return {"message":f"Task{id} updated","status_code":201,"data":t}
+
+    #task not found 
+    raise HTTPException(status_code=404, detail=f"Task {id} not Found")
+
+
+@app.delete('/tasks/{id}')
+async def delete_task(id:uuid.UUID):
+    for t in tasks:
+        if t["id"] == id:
+            tasks.remove(t)
+            return {"message":"No Content",'status_code':201}
+
+    raise HTTPException(status_code=404, detail=f"Unknow id:{id}")
+ 
