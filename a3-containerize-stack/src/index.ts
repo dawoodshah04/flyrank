6b762cb67b0ec413b1f13803ba6a580c64e6efef
dawood shah <dawood0963@gmail.com>
2026-08-12 01:49:29 +0500

    Full CRUD via PostgresDB

---
+++ a3-containerize-stack/src/index.ts
@@ -0,0 +1,133 @@
+import express from "express"
+import dotenv from "dotenv"
+import { getTaskById,
+    createTask,deleteTask
+    ,updateTask,getTasks, 
+    } from "./repository/taskRepository.js";
+import e from "express";
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/tasks", async (_req, res) => {
+  try {
+    const tasks = await getTasks();
+
+    return res.status(200).json(tasks);
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+});
+
+app.post('/tasks',async(_req, res)=>{
+    try {
+        const {title, done=false} = _req.body;
+
+        if ( typeof title !== "string" ||
+      title.trim() === "") {
+            
+            return res.status(400).json({"message":"Title is required"})
+        }
+
+        const task = await createTask(title.trim(), Boolean(done))
+
+        return res.status(200).json({task,"message":"Task created"})
+    } catch (error) {
+        console.log(error)
+        
+        return res.status(500).json({"message":"Internal Server Error"})
+    }
+
+})
+
+
+app.put("/tasks/:id",async (_req, res) => {
+    try {
+        const id = Number(_req.params.id)
+        const { title, done } = _req.body
+
+        if(
+          !Number.isInteger(id) ||
+          typeof title !== "string" ||
+          title.trim() === "" ||
+          typeof done !== "boolean"){
+                return res.status(400).json({error:"Invalid task data"})
+          }
+
+          const task = await updateTask(title.trim(), done, id)
+
+          if(!task){
+            return res.status(400).json({"message":"Task not found"})
+          }
+
+          return res.status(200).json(task)
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error:"Internal Server error"})
+    }
+});
+
+
+app.delete("/tasks/:id",async (_req, res) => {
+   try {
+     const id = Number(_req.params.id);
+     
+        if (!Number.isInteger(id)) {
+       return res.status(400).json({
+         error: "Invalid task id",
+       });
+     }
+ 
+     const deleted = await deleteTask(id);
+ 
+     if(!deleted){
+         return res.status(404).json({
+         error: "Task not found",
+       });
+     }
+ 
+     return res.status(204).send()
+   } catch (error) {
+    console.log(error)
+     return res.status(500).json({error:"Internal Server error"})
+    
+   }
+});
+
+
+app.get("/tasks/:id",async (_req, res) => {
+   try {
+     const id = Number(_req.params.id);
+ 
+       if (!Number.isInteger(id)) {
+        return res.status(400).json({
+          error: "Invalid task id",
+        });
+      }
+ 
+      const taskById = await getTaskById(id);
+ 
+      if(!taskById){
+         return res.status(404).json({error:"Task not found"})
+      }
+ 
+      return res.status(200).json(taskById)
+   } catch (error) {
+    console.log(error)
+     return res.status(500).json({error:"Internal Server error"})
+   }
+})
+
+const PORT = process.env.DATABASE_URL
+ app.listen(PORT,()=>{
+      console.log(`Server running on http://localhost:${PORT}`);
+ })
+
